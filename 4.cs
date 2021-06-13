06233bdec3298513da89b8bbea4f72b95fa0f38d
helloteacher-alt <panyisa20012544@gmail.com>
2021-06-13 17:23:28 +0700

    ข้อ 4

---
+++ 4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ข้อ 4
+            int namecity = Convert.ToInt32(Console.ReadLine());
+            int[] numberCity = new int[namecity];
+            int numbercityAROUND = int.Parse(Console.ReadLine());
+            int numbercityAROUND2 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberCity.Length; i++)
+            {
+                if (numberCity[i] == numbercityAROUND2)
+                {
+                    Console.WriteLine("Invalid ID");
+                }
+            }
+
+
+
+        }
+    }
+}
+
