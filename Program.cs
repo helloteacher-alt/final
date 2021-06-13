7c2093f652300bbc79f09a8d969b143f1914ea85
helloteacher-alt <panyisa20012544@gmail.com>
2021-06-13 17:15:10 +0700

    ข้อ 6

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ข้อ 6
+            int[] nameArray = { 5 };
+            Console.Write("Please input a number from 0 - 3 to choose the following menu:");
+            Console.WriteLine("[0] Show all recent files");
+            Console.WriteLine("[1] Open new file");
+            Console.WriteLine("[2] Open recent file");
+            Console.WriteLine("[3] Exit");
+            Console.Write("input menu:");
+            int menu = int.Parse(Console.ReadLine());
+            if (menu == 0)
+            {
+
+            }
+
+        }
+    }
+}
+
