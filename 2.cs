ef0e3373550bcc0610507c6189c5fc91149690fd
helloteacher-alt <panyisa20012544@gmail.com>
2021-06-13 17:19:17 +0700

    ข้อ 2

---
+++ 2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ข้อ 2
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+            int n5 = int.Parse(Console.ReadLine());
+            int n6 = int.Parse(Console.ReadLine());
+            for (i = size - 1; i > 0; i++);
+
+
+
+        }
+    }
+}
+
