e28d9947728c623b17081a76a519087b307aae30
helloteacher-alt <panyisa20012544@gmail.com>
2021-06-13 17:20:05 +0700

    ข้อ 1

---
+++ 1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ข้อ 1
+            int i, j;
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+            int n5 = int.Parse(Console.ReadLine());
+            int n6 = int.Parse(Console.ReadLine());
+            int n7 = int.Parse(Console.ReadLine());
+            int n8 = int.Parse(Console.ReadLine());
+            int n9 = int.Parse(Console.ReadLine());
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    Console.Write("");
+                }
+            }
+            int n10 = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
+
